28ba78b8d8a65173bab8bdc8c605887b21a00f33
Brendan Higgins <bjh83@case.edu>
2013-08-31 23:47:33 -0400

    I refactored stuff

--- main.cc
+++ main.cc
@@ -12,8 +12,8 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
-    const int width = 100;
-    const int height = 100;
+    const int width = 108;
+    const int height = 108;
     RGBFrame rgb_frame(width, height);
 
     for (int y = 0; y < height; y++) {
